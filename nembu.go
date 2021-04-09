8934a658beb651ceb28c9c5e2eac17f9add763a8
Juan Alvarez <juan@nemphi.com>
2021-04-09 09:02:21 -0500

    Create main `Server` type

--- nembu.go
+++ nembu.go
@@ -1,1 +1,40 @@
 package nembu
+
+import (
+	"sync"
+
+	"github.com/nemphi/nembu-server/config"
+	"github.com/nemphi/nembu-server/database"
+)
+
+type Server struct {
+	cfg         *config.Config
+	db          *database.Connection
+	events      chan Event
+	eventMapper *sync.Map
+	started     bool
+}
+
+func New(options ...Option) (*Server, error) {
+	sv := &Server{
+		events:      make(chan Event),
+		eventMapper: &sync.Map{},
+	}
+	for _, option := range options {
+		err := option(sv)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return sv, nil
+}
+
+func (sv *Server) Start() {
+	sv.listenToEvents()
+	sv.started = true
+}
+
+func (sv *Server) Stop() {
+	close(sv.events)
+	sv.started = false
+}
